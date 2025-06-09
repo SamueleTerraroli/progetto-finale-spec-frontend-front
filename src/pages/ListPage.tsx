fddb48255c0eb0e0f729e9b3268077b1262584c0
SamueleTerraroli <terraroli99@gmail.com>
2025-06-09 15:36:09 +0200

    feat: added search with

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -4,6 +4,21 @@ import type { GPU } from "../types/types";
 
 export default function ListPage() {
     const [gpus, setGpus] = useState<GPU[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [debouncedSearch, setDebouncedSearch] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    const filteredGPUs = gpus.filter(gpu =>
+        gpu.title.toLowerCase().includes(debouncedSearch.toLowerCase()) &&
+        (!selectedCategory || gpu.category.toLowerCase() === selectedCategory.toLowerCase())
+    );
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedSearch(searchTerm);
+        }, 500);
+        return () => clearTimeout(handler);
+
+    }, [searchTerm])
 
     useEffect(() => {
         const getGPUs = async () => {
@@ -20,8 +35,28 @@ export default function ListPage() {
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold text-center mb-4">Lista GPU</h1>
+            <input
+                type="text"
+                placeholder="Cerca GPU..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="border p-2 rounded-md w-full mb-4"
+            />
+
+            <label htmlFor="categorySelect">Categoria:</label>
+            <select
+                id="categorySelect"
+                value={selectedCategory}
+                onChange={(e) => setSelectedCategory(e.target.value)}
+                className="border p-2 rounded-md w-full mb-4"
+            >
+                <option value="">Tutte le categorie</option>
+                <option value="Gaming">Gaming</option>
+                <option value="Budget Gaming">Budget Gaming</option>
+                <option value="Workstation">Workstation</option>
+            </select>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {gpus.map((gpu) => (
+                {filteredGPUs.map((gpu) => (
                     <div key={gpu.id} className="border p-4 rounded-md bg-white shadow-md">
                         <strong className="block text-lg">{gpu.title}</strong>
                         <p className="text-sm text-gray-600">{gpu.brand} - {gpu.vram}GB VRAM</p>
